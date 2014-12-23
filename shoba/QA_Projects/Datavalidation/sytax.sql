981ca912b4ca096249c0dfb2bba7f7af278d1bdc
shobasri <shobasrinath.s@gmail.com>
2014-12-23 02:02:17 -0800

    Revert "commit my files"
    This reverts commit 08e840e99fc403c532ba2323b9564e67f1d60f0a.

--- shoba/QA_Projects/Datavalidation/sytax.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `profile` (
-  `address` varchar(50) DEFAULT NULL,
-  `firstName` varchar(20) DEFAULT NULL,
-  `lastName` varchar(20) DEFAULT NULL,
-  `phoneNumber` bigint(20) DEFAULT NULL,
-  `email` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- shoba/QA_Projects/mysql/db_fetch1.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-db=MySQLdb.connect(host="localhost",user="shoba",passwd="admin",db="test")
-c=db.cursor()
-c.execute("SELECT * FROM testcase_table")
-heads = [d[0] for d in c.description]
-c.fetchall()
-for row in c:
-   print dict(zip(heads,row))
-db.close()
